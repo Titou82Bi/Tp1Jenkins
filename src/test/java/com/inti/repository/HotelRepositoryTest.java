57ee79a3d663196ac4205431ae9c38be74a7ef36
Titou82Bi <inacio.baptiste@gmail.com>
2023-06-29 15:41:06 +0200

    Test

--- src/test/java/com/inti/repository/HotelRepositoryTest.java
+++ src/test/java/com/inti/repository/HotelRepositoryTest.java
@@ -8,10 +8,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.inti.model.Hotel;
 
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class HotelRepositoryTest {
 
 	@Autowired
